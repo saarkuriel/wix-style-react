5c1358746e75054ffa8a3cd1b910a4ff7bff60da
Nitay Rabinovich <nitayr@wix.com>
2019-01-31 13:48:37 +0200

    Initial google analytics implementation

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,6 @@
 import '@storybook/addon-options/register';
 import '@storybook/addon-links/register';
+import '@storybook/addon-google-analytics';
+
+// Set google analytics var;
+window.STORYBOOK_GA_ID = 'UA-133585953-1';
